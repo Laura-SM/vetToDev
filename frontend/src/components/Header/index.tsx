0673d0fb5558f0b0dd50cec6735a19a24015e031
Laura <laura.sabater.musach@gmail.com>
2021-08-06 09:11:17 +0200

    Correct classNames in Header component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,9 +5,9 @@ function Header() {
   return (
     <header className="header">
       <span className="logo">vetToDev</span>
-      <ul className="top-nav">
-        <li className="top-nav__item">home</li>
-        <li className="top-nav__item">blog</li>
+      <ul className="topNavigator">
+        <li className="topNavigator__item">home</li>
+        <li className="topNavigator__item">blog</li>
       </ul>
     </header>
   );
