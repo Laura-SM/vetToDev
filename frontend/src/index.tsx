b61b01b2d78018fbf5350e3132600db04ccddbb3
Laura <laura.sabater.musach@gmail.com>
2021-08-17 17:25:44 +0200

    Add ScrollToTopButton component at index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
+import ScrollToTopButton from './components/ScrollToTopButton';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
 import './styles/global.scss';
@@ -8,6 +9,7 @@ import './styles/global.scss';
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <ScrollToTopButton />
     <Resume />
     <Footer />
   </React.StrictMode>,
