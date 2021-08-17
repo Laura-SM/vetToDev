e8b84de017ee7eb60c3ae57406d191ca56bea600
Laura <laura.sabater.musach@gmail.com>
2021-08-17 16:54:52 +0200

    Create ScrollToTopButton component

---
+++ frontend/src/components/ScrollToTopButton/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './ScrollToTopButton.scss';
+
+function ScrollToTopButton() {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
+  return (
+    <button
+      type="button"
+      onClick={scrollToTop}
+      aria-label="goToTop"
+      style={{ display: visible ? 'inline' : 'none' }}
+    >
+      <span className="material-icons-outlined md-30">
+        arrow_upward
+      </span>
+    </button>
+  );
+}
+
+export default ScrollToTopButton;
