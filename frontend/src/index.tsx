e0ba015dccbf83742f7a22f26b129587f2cc6885
Laura <laura.sabater.musach@gmail.com>
2021-08-17 16:56:52 +0200

    Add Footer component and remove App component in index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Header from './components/Header';
 import Resume from './components/Resume';
+import Footer from './components/Footer';
 import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <Resume />
-    <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
