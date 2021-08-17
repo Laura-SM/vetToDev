40724569a5621c22b93e6408f7037e62fe1e5f6e
Laura <laura.sabater.musach@gmail.com>
2021-08-17 13:02:40 +0200

    Create Competencies component

---
+++ frontend/src/components/Competencies/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import RESUME_DATA from '../../constants/resumeData';
+import './Competencies.scss';
+
+function Competencies() {
+  return (
+    <div className="competencies" id="competencies">
+      <h2>COMPETENCIES</h2>
+      <ul className="competencies__list">
+        {RESUME_DATA.competencies.map((competencie) => (
+          <li className="competencies__item">
+            <span className="material-icons-outlined done">
+              done
+            </span>
+            <span className="competencie">
+              {competencie}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Competencies;
