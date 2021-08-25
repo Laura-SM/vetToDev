da707bbf4f5b89625708896a8bc1875ac3fb2b7f
Laura <laura.sabater.musach@gmail.com>
2021-08-25 13:13:04 +0200

    Rename classNames of Navigator component

--- frontend/src/components/Navigator/index.tsx
+++ frontend/src/components/Navigator/index.tsx
@@ -4,69 +4,69 @@ import './style.scss';
 
 function Navigator() {
   return (
-    <nav className="lateralNavigator">
-      <ul className="lateralNavigator__list">
-        <li className="lateralNavigator__item">
+    <nav className="navigator">
+      <ul className="navigator__list">
+        <li className="navigator__item">
           <a href="#aboutMe">
-            <img className="lateralNavigator__image" src={lauraSabater} alt="Laura Sabater" />
+            <img className="navigator__image" src={lauraSabater} alt="Laura Sabater" />
           </a>
-          <span className="lateralNavigator__tooltip">About me</span>
+          <span className="navigator__tooltip">About me</span>
         </li>
-        <li className="lateralNavigator__item">
+        <li className="navigator__item">
           <a href="#experience">
             <span className="material-icons-outlined md-36">
               business_center
             </span>
           </a>
-          <span className="lateralNavigator__tooltip">Experience</span>
+          <span className="navigator__tooltip">Experience</span>
         </li>
-        <li className="lateralNavigator__item">
+        <li className="navigator__item">
           <a href="#education">
             <span className="material-icons-outlined">
               school
             </span>
           </a>
-          <span className="lateralNavigator__tooltip">Education</span>
+          <span className="navigator__tooltip">Education</span>
         </li>
-        <li className="lateralNavigator__item">
+        <li className="navigator__item">
           <a href="#competencies">
             <span className="material-icons-outlined md-36">
               verified
             </span>
           </a>
-          <span className="lateralNavigator__tooltip">Competencies</span>
+          <span className="navigator__tooltip">Competencies</span>
         </li>
-        <li className="lateralNavigator__item">
+        <li className="navigator__item">
           <a href="#languages">
             <span className="material-icons-outlined md-36">
               language
             </span>
           </a>
-          <span className="lateralNavigator__tooltip">Languages</span>
+          <span className="navigator__tooltip">Languages</span>
         </li>
-        <li className="lateralNavigator__item">
+        <li className="navigator__item">
           <a href="#awards">
             <span className="material-icons-outlined md-36">
               emoji_events
             </span>
           </a>
-          <span className="lateralNavigator__tooltip">Awards</span>
+          <span className="navigator__tooltip">Awards</span>
         </li>
-        <li className="lateralNavigator__item">
+        <li className="navigator__item">
           <a href="#blog">
             <span className="material-icons-outlined md-36">
               speaker_notes
             </span>
           </a>
-          <span className="lateralNavigator__tooltip">Blog</span>
+          <span className="navigator__tooltip">Blog</span>
         </li>
-        <li className="lateralNavigator__item">
+        <li className="navigator__item">
           <a href="#contact">
             <span className="material-icons-outlined md-36">
               email
             </span>
           </a>
-          <span className="lateralNavigator__tooltip">Contact</span>
+          <span className="navigator__tooltip">Contact</span>
         </li>
       </ul>
     </nav>
